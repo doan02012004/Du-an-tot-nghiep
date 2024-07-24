7e22c653186916fa3055605042bf6d08ec527143
unknown <trunghieu2004x7@gmail.com>
2024-07-24 15:29:10 +0700

    update-address

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,8 +2,8 @@ import express from 'express'
 import { deleteUser, getAllUser, getByIdUser, updateUser } from '../controllers/userController.js'
 const router = express.Router()
 
-router.get('/user',getAllUser)
-router.get('/user/:id',getByIdUser)
-router.delete('/user/delete/:id',deleteUser)
-router.put('/user/update/:id',updateUser)
+router.get('/',getAllUser)
+router.get('/:id',getByIdUser)
+router.delete('/delete/:id',deleteUser)
+router.put('/update/:id',updateUser)
 export default router
