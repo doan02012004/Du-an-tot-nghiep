ccdc1b81837d5344e851fb0824bdd3e854d05900
doan02012004 <doana1k24@gmail.com>
2024-07-30 15:14:14 +0700

    complete home and information

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -2,11 +2,19 @@
 import { Route, Routes } from 'react-router-dom'
 import LayoutWebsite from '../pages/(website)/LayoutWebsite'
 import LayoutAdmin from '../pages/(admin)/LayoutAdmin'
+import HomePage from '../pages/(website)/home/HomePage'
+import MyInformation from '../pages/(website)/my-information/Page'
+import Account from '../pages/(website)/my-information/information/Page'
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<LayoutWebsite />}></Route>
+      <Route path='/' element={<LayoutWebsite />}>
+          <Route index element={<HomePage />} />
+          <Route path='customer' element={<MyInformation />}>
+                <Route path='infor' element={<Account />}/>
+          </Route>
+      </Route>
       <Route path='admin' element={<LayoutAdmin />}></Route>
     </Routes>
   )
