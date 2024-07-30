d93a55c26188661fe3c81e682cb282de69482d55
unknown <trunghieu2004x7@gmail.com>
2024-07-30 15:49:23 +0700

    complete cart and order

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -2,11 +2,16 @@
 import { Route, Routes } from 'react-router-dom'
 import LayoutWebsite from '../pages/(website)/LayoutWebsite'
 import LayoutAdmin from '../pages/(admin)/LayoutAdmin'
+import CartPage from '../pages/(website)/cart/CartPage'
+import OrderPage from '../pages/(website)/order/OrderPage'
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<LayoutWebsite />}></Route>
+      <Route path='/' element={<LayoutWebsite />}>
+        <Route path='cart' element={<CartPage/>}/>
+        <Route path='order' element={<OrderPage/>}/>
+      </Route>
       <Route path='admin' element={<LayoutAdmin />}></Route>
     </Routes>
   )
