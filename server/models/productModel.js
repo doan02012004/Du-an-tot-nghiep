f609fa88fe6df64d75a3480a826bc5388035c99c
doan02012004 <doana1k24@gmail.com>
2024-07-31 15:17:53 +0700

    update productSchema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
+const gallerySchema = new mongoose.Schema({
+    imageUrl:{type:String},
+    name:{type:String},
+})
 const attributeSchema = new mongoose.Schema({
     size:{type:String},
     color:{type:String},
-    image:{type:String},
-    instock:{type:Number,default:0}
-},{
-    _id:true, timestamps:true,versionKey:false
+    instock:{type:Number,default:0},
+    background:{type:String},
 })
-
 const productShema = new mongoose.Schema({
     name:{type:String, required:true},
     categoryId:{type:mongoose.Schema.Types.ObjectId, ref:'categories'},
     price_old:{type:Number, required:true},
     discount:{type:Number, default:0},
+    gallerys:[gallerySchema],
     gender:{type:String, enum:['male',"female","unisex"],default:"unisex"},
     description:{type:String, default:""},
     featured:{type:Boolean,default:false},
     status:{type:Boolean, default:true},
-    attributes:[attributeSchema]
+    attributes:[attributeSchema],
+    purchases:{type:Number, default:0},
+    comment:[]
 },{
     timestamps:true , versionKey:false
 })
