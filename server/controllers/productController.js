b54f92695ffa41e71181928d38637f65af623654
doan02012004 <doana1k24@gmail.com>
2024-07-31 15:47:21 +0700

    31/07 update productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -32,7 +32,7 @@ export const getAllProduct = async(req, res)=>{
     //   if (maxPrice) {
     //     query.price = { ...query.price, $lte: Number(maxPrice) };
     //   }
-        const products = await ProductModel.find().populate('categoryId')
+        const products = await ProductModel.find()
         return res.status(200).json(products)
     } catch (error) {
         return res.status(500).json({
