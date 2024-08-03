222d970fc21331c6da64eba153ae9483eaccd2c4
Anh10 <anhnguyen1524@gmail.com>
2024-08-03 15:13:19 +0700

    update

--- client/src/pages/(admin)/product/add/Page.tsx
+++ client/src/pages/(admin)/product/add/Page.tsx
@@ -1,8 +1,98 @@
-import React from "react";
-import { Button, Checkbox, Form, Input, InputNumber } from "antd";
+// const color ={
+//   _id:"djfhjdhjdh",
+//   name:"ĐEN",
+//   background:"black"
+// }
+// const product = {
+//   _id: 'dfhjdfhj',
+//   categoryId:{
+//     _id:'kjdfkdkfj',
+//     name:"Áo sơ mi",
+//     slug:"ao-so-mi"
+//   },
+//   name: "Áo sơ mi",
+//   price_old: 150000,
+//   discount: 10,
+//   description: "Mô tả",
+//   active: true,
+//   featured: true,
+//   gender:"unisex",
+//   gallerys: [
+//     {
+//       _id:'hhfdjhdj',
+//       colorId:"djfkd",
+//       avatar: "http:....",
+//       items: [
+//         "http://picsum...",
+//         "http://picsum...",
+//         "http://picsum...",
+//         "http://picsum...",
+//       ]
+//     },
+//     {
+//       _id:'hhfdjhdj',
+//       name: "TRẮNG",
+//       background: "black",
+//       avatar: "http:....",
+//       items: [
+//         "http://picsum...",
+//         "http://picsum...",
+//         "http://picsum...",
+//         "http://picsum...",
+//       ]
+//     },
+//   ],
+//   attributes:[
+//     {
+//       _id:'djhjdfhj',
+//       size:"M",
+//       color:"ĐEN",
+//       instock:10
+//     },
+//     {
+//       _id:'djhjdfhj',
+//       size:"M",
+//       color:"TRẮNG",
+//       instock:10
+//     },
+//     {
+//       _id:'djhjdfhj',
+//       size:"L",
+//       color:"ĐEN",
+//       instock:10
+//     },
+//     {
+//       _id:'djhjdfhj',
+//       size:"L",
+//       color:"TRẮNG",
+//       instock:10
+//     },
+//   ]
+// }
+
+// import styles
+import { Tabs } from "antd";
+import FormInfor from "./_components/FormInfor";
+import Properties from "./_components/Properties";
 
 const AddProductAdmin = () => {
-  return <div></div>;
-};
+  const items = [
+    {
+      key: '1',
+      label: 'Thông tin sản phẩm',
+      children: <FormInfor />
+    },
+    {
+      key: '2',
+      label: 'Biến thể',
+      children: <Properties />,
+    },
+    
+  ]
+ 
+  return (
+   <Tabs items={items} defaultActiveKey="1"/>
+  )
+}
 
-export default AddProductAdmin;
+export default AddProductAdmin
