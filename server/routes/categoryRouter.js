f0ff0285c1c5197588f1fcea9f7439018fd1b140
doan02012004 <doana1k24@gmail.com>
2024-08-03 23:35:44 +0700

    update router

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { create, getAll } from "../controllers/categoryController.js";
+import { create, deleteCategoryById, getAll, getCategoryById, updateCategoryById } from "../controllers/categoryController.js";
 
 
 const router = Router();
 router.get("/", getAll);
-;
 router.post("/", create);
-
 router.get("/:id", getCategoryById);
 router.delete("/:id", deleteCategoryById);
 router.put("/:id", updateCategoryById)
