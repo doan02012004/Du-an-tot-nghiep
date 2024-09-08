6859aeaa66bb4bca39546563c845f772c3ee56ef
HoangKyPham <hoangkypham1682@gmail.com>
2024-09-08 21:23:03 +0700

    kyph/8-9/filter-product

--- client/src/pages/(website)/productdetails/_components/Breadcrumb_products.tsx
+++ client/src/pages/(website)/productdetails/_components/Breadcrumb_products.tsx
@@ -12,8 +12,8 @@ const Breadcrumb_products = ({product}: Props) => {
                   <section className="breadcrumb-products container lg:mb-[43px] mb-4">
                   <ol className="mt-[16px] mb-[16px] flex text-[12px] text-[#6c6d70] lg:text-[14px] lg:mt-[23px] lg:mb-[23px] lg:pl-[15px]">
                   <li><a href="#">Trang chủ</a></li><span className="ml-1 mr-1">-</span>
-                  <li><a href="#">{product.categoryId.name}</a></li><span className="ml-1 mr-1">-</span>
-                  <li><a href="#">{product.name}</a></li>
+                  <li><a href="#">{product?.categoryId?.name}</a></li><span className="ml-1 mr-1">-</span>
+                  <li><a href="#">{product?.name}</a></li>
                   </ol>
                   <div><hr /></div>
           </section>
