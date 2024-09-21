4f4e1f2749c8d048e9472edc1c72911e07f22826
Zinop151204 <huyt20600@gmail.com>
2024-09-21 22:55:53 +0700

    update location api online

--- client/src/common/contexts/AppContextProvider.tsx
+++ client/src/common/contexts/AppContextProvider.tsx
@@ -5,20 +5,21 @@ import { createContext, ReactNode, useEffect, useState } from 'react'
 import instance from '../config/axios'
 import { getAccountUser, getNewToken } from '../../services/auth'
 import useLocalStorage from '../hooks/localstorage/useLocalStorage'
+import useApiLocationQuery from '../hooks/API_location/useApiLocationQuery'
 
 type AppContextProviderProps = {
   children: ReactNode
 }
 
-const fetchUser = async(setCurrentUser?:any,setIsLogin?:any,setIsLoading?:any)=>{
+const fetchUser = async (setCurrentUser?: any, setIsLogin?: any, setIsLoading?: any) => {
   setIsLoading(true)
   try {
     const user = await getAccountUser()
-   
-    if(user){
+
+    if (user) {
       setCurrentUser(user)
       setIsLogin(true)
-      
+
     }
     setIsLoading(false)
   } catch (error) {
@@ -33,17 +34,19 @@ export const AppContext = createContext<any>(null)
 const AppContextProvider = ({ children }: AppContextProviderProps) => {
   const [choiceColor, setChoiceColor] = useState('')
   const [collapsed, setCollapsed] = useState(false);
-  const [currentUser, setCurrentUser] = useLocalStorage('tt_user',{})
+  const [currentUser, setCurrentUser] = useLocalStorage('tt_user', {})
   const [isLoading, setIsLoading] = useState(false)
+  const locationQuery = useApiLocationQuery()
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   const [accessToken, setAccesToken] = useLocalStorage('accessToken', null)
-  const [isLogin,setIsLogin] = useLocalStorage('login',null)
+  const [isLogin, setIsLogin] = useLocalStorage('login', null)
+  const [location, setLocation] = useLocalStorage('location', null)
   // Interceptor request axios
   useEffect(() => {
     // Thêm một request interceptor
-    const requestInterceptor = instance.interceptors.request.use(function (config:any) {
+    const requestInterceptor = instance.interceptors.request.use(function (config: any) {
       // // lệnh thực thi trước khi gửi đi 1 request
       //  console.log("accessToken Rq >> :",accessToken)
       config.headers.Authorization = !config._retry && accessToken ? `Bearer ${accessToken}` : config.headers.Authorization;
@@ -101,20 +104,24 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
 
   // get user
   useEffect(() => {
-  // if(accessToken === undefined || accessToken === undefined && isLogin == false || isLogin === null ||isLogin === undefined ||isLogin === true && !currentUser) {
-  //   fetchUser(setCurrentUser,setIsLogin,setIsLoading)
-    
-  // }
-  if(accessToken || accessToken == undefined){
-    fetchUser(setCurrentUser,setIsLogin,setIsLoading)
-  }else{
-    setIsLogin(false)
-    setCurrentUser({})
-    console.log('logout fail')
-  }
-  },[accessToken])
+    if (accessToken || accessToken == undefined) {
+      fetchUser(setCurrentUser, setIsLogin, setIsLoading)
+    } else {
+      setIsLogin(false)
+      setCurrentUser({})
+      console.log('logout fail')
+    }
+  }, [accessToken])
+
+//set địa chỉ 
+  useEffect(() => {
+    if (locationQuery.data) {
+      setLocation(locationQuery.data)
+    }
+  }, [locationQuery.data])  
+
   return (
-    <AppContext.Provider value={{collapsed, setCollapsed, colorBgContainer, borderRadiusLG, accessToken, setAccesToken,setIsLogin,isLogin,isLoading,currentUser,choiceColor,setChoiceColor}}>
+    <AppContext.Provider value={{ collapsed, setCollapsed, colorBgContainer, borderRadiusLG, accessToken, setAccesToken, setIsLogin, isLogin, isLoading, currentUser, choiceColor, setChoiceColor, location }}>
       {children}
     </AppContext.Provider>
   )
