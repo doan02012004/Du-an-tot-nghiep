6520d5282ba43acb4a91f8d27959f2c25f3caf31
NgocHao8686 <nhao86204@gmail.com>
2024-09-27 15:19:23 +0700

    merge order

--- client/src/pages/(website)/cart/CartPage.tsx
+++ client/src/pages/(website)/cart/CartPage.tsx
@@ -1,23 +1,33 @@
 
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Status from './_components/Status'
 import Table from './_components/Table'
 import Total from './_components/Total'
 import useCartQuery from '../../../common/hooks/carts/useCartQuery'
+import { message } from 'antd'
 
 const CartPage = () => {
-    const {data:cartUser} = useCartQuery()
+    const { data: cartUser } = useCartQuery()
+    const navigate = useNavigate()
+    const onHandleCheckToOrder = () => {
+        if (!cartUser || cartUser?.totalCart == 0) {
+            return message.error("Hiện tại chưa có sản phẩm nào")
+        }
+        else {
+            return navigate("/order")
+        }
+    }
     return (
         <section>
             <div>
                 <div className="container mx-auto lg:flex pt-11 pb-4 lg:gap-10">
                     <div className="lg:w-[68%]">
-                        <Status/>
+                        <Status />
                         <div className="py-6 lg:py-11 flex gap-2 items-center">
                             <span className="text-sm lg:text-2xl text-[#000000] font-bold">Giỏ hàng của bạn</span>
-                            <span className="text-sm lg:text-2xl text-red font-bold">{cartUser?cartUser?.totalCart:"0"} Sản Phẩm</span>
+                            <span className="text-sm lg:text-2xl text-red font-bold">{cartUser ? cartUser?.totalCart : "0"} Sản Phẩm</span>
                         </div>
-                        <Table cartUser={cartUser}/>
+                        <Table cartUser={cartUser} />
                         <div className="py-6 lg:pt-7">
                             <a href="/product" className="w-[60%] flex items-center border border-black lg:w-[30%] py-3 justify-center gap-2 rounded-tl-[20px] rounded-br-[20px] hover:bg-black hover:text-white">
                                 <span><i className="fa-solid fa-arrow-left-long" /></span>
@@ -26,8 +36,8 @@ const CartPage = () => {
                         </div>
                     </div>
                     <div className="lg:w-[32%]">
-                        <Total cartUser={cartUser}/>
-                        <Link to={'/order'}><button className="bg-black text-white w-full py-3 text-lg font-semibold rounded-tl-[20px] rounded-br-[20px] hover:bg-white hover:text-black hover:border hover:border-black">ĐẶT HÀNG</button></Link>
+                        <Total cartUser={cartUser} />
+                        <button onClick={onHandleCheckToOrder} className="bg-black text-white w-full py-3 text-lg font-semibold rounded-tl-[20px] rounded-br-[20px] hover:bg-white hover:text-black hover:border hover:border-black">ĐẶT HÀNG</button>
                     </div>
                 </div>
             </div>
