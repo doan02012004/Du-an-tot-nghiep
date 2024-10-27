8cbfc4c2dfa39df37f8667c165bd108aa40453dd
HoangKyPham <hoangkypham1682@gmail.com>
2024-10-27 21:41:41 +0700

    kyph/27-10/update-select-address-order

--- client/src/pages/(website)/order/_components/OrderAddressItem.tsx
+++ client/src/pages/(website)/order/_components/OrderAddressItem.tsx
@@ -5,32 +5,38 @@ import OrderListAddress from './OrderListAddress'
 
 type Props = {
     address: Iaddress,
-    listAddress : Iaddress[]
+    listAddress: Iaddress[]
 }
 
 const OrderAddressItem = ({ address, listAddress }: Props) => {
     const [isOpenAddress, setIsOpenAddress] = useState(false)
-
-
-    console.log(isOpenAddress)
+    const [addressOrder, setAddressOrder] = useState(address)
 
     return (
         <div className=" py-6">
             <div className="my-4 border rounded-tl-[30px] rounded-br-[30px]">
                 <div className="px-5 py-6 lg:py-8 lg:px-10 justify-between">
                     <div className="flex items-center justify-between">
-                        <span className="text-lg text-black font-semibold">{address?.fullname} {address.option == "house" ? "(Nhà riêng)" : "(Cơ quan)"}</span>
+                        <span className="text-lg text-black font-semibold">{addressOrder?.fullname} {addressOrder.option == "house" ? "(Nhà riêng)" : "(Cơ quan)"}</span>
                         <div className="flex items-center gap-2">
                             <button onClick={() => setIsOpenAddress(!isOpenAddress)} className='underline'><u>Chọn địa chỉ khác</u></button>
-                            <button disabled className="bg-black rounded-tl-[10px] cursor-pointer rounded-br-[10px] px-4 py-2 text-white hover:bg-white hover:text-black hover:border hover:border-black">MẶC ĐỊNH</button>
+                            {addressOrder.isDefault && (
+                                <button disabled className="bg-black rounded-tl-[10px] cursor-pointer rounded-br-[10px] px-4 py-2 text-white hover:bg-white hover:text-black hover:outline hover:outline-1 hover:outline-black">MẶC ĐỊNH</button>
+                            )}
                         </div>
                     </div>
                     {isOpenAddress && (
-                        <OrderListAddress listAddress={listAddress} isOpenAddress={isOpenAddress}/>
+                        <OrderListAddress
+                            addressOrder={addressOrder}
+                            setAddressOrder={setAddressOrder}
+                            listAddress={listAddress}
+                            setIsOpenAddress={setIsOpenAddress}
+                        />
                     )}
+
                     <div className="flex flex-col gap-2">
-                        <span className='text-sm'>Điện thoại: {address?.phone}</span>
-                        <span className='text-sm'>Địa chỉ: {address.address}, {address?.district}, {address?.city}</span>
+                        <span className='text-sm'>Điện thoại: {addressOrder?.phone}</span>
+                        <span className='text-sm'>Địa chỉ: {addressOrder.address}, {addressOrder?.district}, {addressOrder?.city}</span>
                     </div>
                 </div>
             </div>
--- client/src/pages/(website)/order/_components/OrderListAddress.tsx
+++ client/src/pages/(website)/order/_components/OrderListAddress.tsx
@@ -4,22 +4,22 @@ import closeIcon from '../../../../assets/icons/x-letter.svg'
 
 
 type Props = {
+    addressOrder: any,
     listAddress: Iaddress[],
-    isOpenAddress : boolean
+    setIsOpenAddress: any,
+    setAddressOrder: any
 }
 
-const OrderListAddress = ({ listAddress, isOpenAddress }: Props) => {
+const OrderListAddress = ({ addressOrder, setAddressOrder, listAddress, setIsOpenAddress }: Props) => {
 
-    const aa= false;
-    
     return (
-        <div className='fixed top-0 left-0 bg-black/30 w-full h-full z-50 flex justify-center'>
+        <div onClick={() => setIsOpenAddress(false)} className='fixed top-0 left-0 bg-black/30 w-full h-full z-50 flex justify-center'>
             <div className=" py-6">
                 <div className="my-4 rounded-md  w-[320px] lg:w-[700px] pt-6 bg-white">
                     <div className="flex justify-between">
                         <div className="mx-6 mb-4">Chọn địa chỉ</div>
                         <div className="mx-6 mb-4">
-                            <button onClick={() => isOpenAddress = aa}>
+                            <button onClick={() => setIsOpenAddress(false)}>
                                 <img src={closeIcon} alt="" srcSet="" />
                             </button>
                         </div>
@@ -27,20 +27,30 @@ const OrderListAddress = ({ listAddress, isOpenAddress }: Props) => {
                     <div className=" w-[100%] bg-slate-400 h-[1px]"></div>
                     <div className="px-6 py-6">
                         {listAddress && listAddress.map(address => (
-                            <div className="px-5 mt-4 rounded-tl-[30px] rounded-br-[30px] border border-green-500 py-6 lg:py-8 lg:px-10 justify-between">
+                            <div
+                                key={address._id}
+                                onClick={() => setAddressOrder(address)}
+                                className={`px-5 cursor-pointer mt-4 rounded-tl-[30px] rounded-br-[30px] border py-6 lg:py-8 lg:px-10 justify-between
+                ${addressOrder._id === address._id ? 'border-green-500' : 'border-gray-300'}`}
+                            >
                                 <div className="flex items-center justify-between">
-                                    <span className="text-lg text-black font-semibold">{address?.fullname} {address.option == "house" ? "(Nhà riêng)" : "(Cơ quan)"}</span>
+                                    <span className="text-lg text-black font-semibold">
+                                        {address?.fullname} {address.option === "house" ? "(Nhà riêng)" : "(Cơ quan)"}
+                                    </span>
                                     <div className="flex items-center gap-2">
-                                        <button disabled className="bg-black rounded-tl-[10px] cursor-pointer rounded-br-[10px] px-4 py-2 text-white hover:bg-white hover:text-black hover:border hover:border-black">MẶC ĐỊNH</button>
+                                        {address.isDefault && (
+                                            <button disabled className="bg-black rounded-tl-[10px] cursor-pointer rounded-br-[10px] px-4 py-2 text-white hover:bg-white hover:text-black hover:outline hover:outline-1 hover:outline-black">MẶC ĐỊNH</button>
+                                        )}
                                     </div>
                                 </div>
-                                <div className="flex  flex-col gap-2">
-                                    <span className='text-sm'>Điện thoại: {address?.phone}</span>
-                                    <span className='text-sm'>Địa chỉ: {address.address}, {address?.district}, {address?.city}</span>
+                                <div className="flex flex-col gap-2">
+                                    <span className="text-sm">Điện thoại: {address?.phone}</span>
+                                    <span className="text-sm">Địa chỉ: {address.address}, {address?.district}, {address?.city}</span>
                                 </div>
                             </div>
                         ))}
                     </div>
+
                 </div>
             </div>
         </div>
