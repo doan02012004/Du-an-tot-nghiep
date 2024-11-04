c83a277838167fd31d50f985df0ac19fb09dc5c5
HoangKyPham <hoangkypham1682@gmail.com>
2024-11-05 00:42:28 +0700

    kyph/5-11/update-paginate-product

--- client/src/pages/(website)/product/_components/Pagination.tsx
+++ client/src/pages/(website)/product/_components/Pagination.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useFilterParams } from '../../../../common/hooks/products/useFilter'
-import { useNavigate, useSearchParams } from 'react-router-dom'
 
 type Props = {
   dataPage: {
@@ -10,40 +9,70 @@ type Props = {
 }
 
 const Pagination = ({ dataPage }: Props) => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const { setFilterParams, getFiltersFromUrl } = useFilterParams();
+  const { setFilterParams,} = useFilterParams();
   const navigate = useNavigate();
 
 
   const handleApply = (pageData: any) => {
-    console.log(pageData)
+    console.log(pageData);
     const params = setFilterParams({ page: pageData });
     navigate(`?${params?.toString()}`);
   };
 
   return (
     <>
-      <ul className="list-inline-pagination flex justify-center items-center lg:mt-[70px]  mt-[32px]">
+      <ul className="list-inline-pagination flex justify-center items-center lg:mt-[70px] mt-[32px]">
         {dataPage?.currentPage > 1 && (
-          <li onClick={() => handleApply(1)} className="mr-[17px] lg:mx-2 hidden lg:block"><span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white  rounded-tl-lg rounded-br-lg">Trang đầu</span></li>
+          <li onClick={() => handleApply(1)} className="mr-[17px] lg:mx-2 hidden lg:block">
+            <span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white rounded-tl-lg rounded-br-lg">
+              Trang đầu
+            </span>
+          </li>
         )}
-        <li onClick={() => { if (dataPage?.currentPage > 1) { handleApply(dataPage?.currentPage - 1) } }} className="mr-[17px] lg:mx-2"><span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white  rounded-tl-lg rounded-br-lg">«</span></li>
-        {Array.from({ length: dataPage?.totalPage }, (_, i) => (
-          <li key={i} onClick={() => { if (dataPage?.currentPage !== i + 1) { handleApply(i + 1) } }} className="mr-[17px] lg:mx-2"><span className={`${dataPage?.currentPage == i + 1 ? 'bg-black text-white' : 'bg-white '} block cursor-pointer py-2 px-4 border hover:bg-black hover:text-white  rounded-tl-lg rounded-br-lg`}>{i + 1}</span></li>
-        ))}
+
+        <li onClick={() => {
+          if (dataPage?.currentPage > 1) {
+            handleApply(dataPage?.currentPage - 1);
+          }
+        }} className="mr-[17px] lg:mx-2">
+          <span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white rounded-tl-lg rounded-br-lg">«</span>
+        </li>
+
+        {Array.from({ length: Math.min(5, dataPage?.totalPage) }, (_, i) => {
+          const startPage = Math.max(1, Math.min(dataPage?.currentPage - 2, dataPage?.totalPage - 4));
+          const page = startPage + i;
+
+          return (
+            <li key={page} onClick={() => {
+              if (dataPage?.currentPage !== page) {
+                handleApply(page);
+              }
+            }} className="mr-[17px] lg:mx-2">
+              <span className={`${dataPage?.currentPage == page ? 'bg-black text-white' : 'bg-white'} block cursor-pointer py-2 px-4 border hover:bg-black hover:text-white rounded-tl-lg rounded-br-lg`}>
+                {page}
+              </span>
+            </li>
+          );
+        })}
         <li onClick={() => {
           if (dataPage?.currentPage < dataPage?.totalPage) {
-            handleApply(Math.min(dataPage?.currentPage + 1, dataPage?.totalPage));
+            
+            handleApply(Math.min(Number(dataPage?.currentPage) + 1, dataPage?.totalPage));
           }
-        }}
-          className="mr-[17px] lg:mx-2">
+        }} className="mr-[17px] lg:mx-2">
           <span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white rounded-tl-lg rounded-br-lg">»</span>
         </li>
 
-        {dataPage?.currentPage !== dataPage?.totalPage && (
-          <li onClick={() => { handleApply(dataPage?.totalPage) }} className="mr-[17px] lg:mx-2 hidden lg:block"><span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white  rounded-tl-lg rounded-br-lg">Trang cuối</span></li>
+
+        {Number(dataPage?.currentPage) !== dataPage?.totalPage && (
+          <li onClick={() => handleApply(dataPage?.totalPage)} className="mr-[17px] lg:mx-2 hidden lg:block">
+            <span className="block cursor-pointer py-2 px-4 bg-white border hover:bg-black hover:text-white rounded-tl-lg rounded-br-lg">
+              Trang cuối
+            </span>
+          </li>
         )}
       </ul>
+
     </>
   )
 }
