b66a92f7a47b35bb97791eaed7c89ea75d60a287
HoangKyPham <hoangkypham1682@gmail.com>
2024-11-30 00:27:43 +0700

    kyph/30-11/search-recent

--- client/src/pages/(website)/_components/QuicklySearch.tsx
+++ client/src/pages/(website)/_components/QuicklySearch.tsx
@@ -18,20 +18,35 @@ const QuicklySearch = () => {
     // console.log(searchHistoryQuery)
     const inputRef = useRef<HTMLInputElement | null>(null); // Thêm useRef cho input
     const keywordUrl = searchParams.get('search')
-    
+
+
     useEffect(() => {
         if (keywordUrl) {
             reset({ keyword: keywordUrl })
         }
     }, [keywordUrl])
+
+    const saveSearch = (query: any) => {
+        const searches = JSON.parse(localStorage.getItem("searches") || "[]") as any[];
+        searches.unshift(query); // Thêm tìm kiếm mới
+        localStorage.setItem("searches", JSON.stringify(searches));
+    };
+
+
+    const dataSearchRecent: any = JSON.parse(localStorage.getItem("searches") || "[]");
+
     const onSearch = (data: any) => {
         const { keyword } = data
+        console.log(keyword)
         if (currentUser) {
             trackSearchMutation.mutate({ action: 'add', keyword, userId: currentUser._id })
         }
         navigate(`/timkiem?search=${keyword}`)
+        saveSearch(keyword)
         inputRef.current?.blur(); // Mất focus sau khi điều hướng
     }
+
+
     return (
         <div className="search-wrapper content-menu-bottom fixed z-[13]  top-0 left-0 w-full h-screen hidden bg-white lg:w-auto lg:block lg:h-auto lg:px-0 lg:static lg:z-auto">
             <div className=" mt-3 relative mx-[15px] lg:mr-5 lg:block group lg:mx-0 lg:mt-0 ">
@@ -61,8 +76,12 @@ const QuicklySearch = () => {
                         </div>
                         <h1 className="mb-4 text-sm text-[#221F20] font-semibold">Vừa tìm kiếm</h1>
                         <div className="flex items-center flex-wrap gap-3 ">
-                            <a href="#" className="block px-2 py-2 border border-gray-200 text-[12px]/[150%] rounded-tl-lg rounded-br-lg transition duration-300 ease-in-out hover:text-white hover:bg-black">Áo
-                                polo</a>
+                            {
+                                dataSearchRecent.slice(0, 11).map((search: string) => (
+                                    <Link to={`timkiem?search=${search}`} className="block px-2 py-2 border border-gray-200 text-[12px]/[150%] rounded-tl-lg rounded-br-lg transition duration-300 ease-in-out hover:text-white hover:bg-black">{search}</Link>
+
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
